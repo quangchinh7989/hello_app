421a104ede229e370a172746dde0b455e67d64b7
Quang Chinh <lechinh.sv.ktktcn@gmail.com>
2018-12-14 14:21:13 +0700

    Update Gemfile for Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require rails-ujs
 //= require jquery3
+//= require jquery-ujs
+//= require jquery-ui
+//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
